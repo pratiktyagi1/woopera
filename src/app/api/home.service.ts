afc0cef72db487e1f27118cf92baec31fecda4b8
Raj <raj.uimatic@gmail.com>
2020-02-24 10:37:28 +0530

    First Commit

---
+++ src/app/api/home.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class HomeService {
+  static httpClient: any;
+  constructor(private httpClient: HttpClient) { }
+  public getHomeFilterData() {
+    return this.httpClient.get('http://woopera.quintsofts.com/api/getoperas/en');
+    // throw new Error("Method not implemented.");
+  }
+}
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../api/home.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  filterData: any = {};
+  opera: any[] = [];
+  constructor(public homeService: HomeService) {}
+
+  ngOnInit() {
+    // tslint:disable-next-line: variable-name
+    this.homeService.getHomeFilterData().subscribe((data) => {
+      this.filterData = data;
+      console.log(this.filterData.opera)
+      // this.opera = data.opera;
+    });
+  }
+
+}
